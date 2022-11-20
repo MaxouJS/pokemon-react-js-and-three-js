db77cf5043c184cdc8986a9820bec274a4200be8
MaxenceGumiero <117424624+MaxouJS@users.noreply.github.com>
2022-11-20 16:07:11 +1100

    Improving Desert Generator

--- src/components/utils/DesertGenerator.tsx
+++ src/components/utils/DesertGenerator.tsx
@@ -13,8 +13,10 @@ import Props from '../3d/Props';
 const DesertGenerator: FC = () => {
   // States
   const [cliffYRotation, setCliffYRotation]: [number | null, Dispatch<SetStateAction<number | null>>] = useState<number | null>(null);
-  const [rocks, setRocks]: [PropsType[] | null, Dispatch<SetStateAction<PropsType[] | null>>] = useState<PropsType[] | null>(null);
+  const [elements, setElements]: [AnimationType[] | PropsType[] | null, Dispatch<SetStateAction<AnimationType[] | PropsType[] | null>>] = useState<AnimationType[] | PropsType[] | null>(null);
   const [cactus, setCactus]: [AnimationType[] | null, Dispatch<SetStateAction<AnimationType[] | null>>] = useState<AnimationType[] | null>(null);
+  const [dunes, setDunes]: [PropsType[] | null, Dispatch<SetStateAction<PropsType[] | null>>] = useState<PropsType[] | null>(null);
+  const [rocks, setRocks]: [PropsType[] | null, Dispatch<SetStateAction<PropsType[] | null>>] = useState<PropsType[] | null>(null);
 
   useEffect((): void => {
     // Sets cliff Y axis rotation randomly
@@ -22,6 +24,7 @@ const DesertGenerator: FC = () => {
 
     // Initializes empty array of 3D props
     const newCactus: AnimationType[] = [];
+    const newDunes: PropsType[] = [];
     const newRocks: PropsType[] = [];
 
     const creatingElements = (
@@ -33,20 +36,20 @@ const DesertGenerator: FC = () => {
       maxScale: number,
       array: AnimationType[] | PropsType[],
     ): void => {
-      // Creates 100 rocks with random position, rotation and scale
+      // Creates N elements with random position, rotation and scale
       for (let index = 0; index < elements; index++) {
-        let randomXPosition = Math.floor(Math.random() * distance - (distance / 2));
+        let randomXPosition = Math.random() * distance - (distance / 2);
 
-        // Checks if a rock can be in the X axis position of a Pokémon, then reassigns the X axis of the rock if it's the case
+        // Checks if a element can be in the X axis position of a Pokémon, then reassigns the X axis of the element if it's the case
         while (randomXPosition >= -10 && randomXPosition <= 10) {
-          randomXPosition = Math.floor(Math.random() * distance - (distance / 2));
+          randomXPosition = Math.random() * distance - (distance / 2);
         }
 
-        let randomZPosition = Math.floor(Math.random() * distance - (distance / 2));
+        let randomZPosition = Math.random() * distance - (distance / 2);
 
-        // Checks if a rock can be in the Z axis position of a Pokémon, then reassigns the Z axis of the rock if it's the case
+        // Checks if a element can be in the Z axis position of a Pokémon, then reassigns the Z axis of the element if it's the case
         while (randomZPosition >= -10 && randomZPosition <= 10) {
-          randomZPosition = Math.floor(Math.random() * distance - (distance / 2));
+          randomZPosition = Math.random() * distance - (distance / 2);
         }
 
         // Creates a random position, rotation and scale
@@ -63,6 +66,7 @@ const DesertGenerator: FC = () => {
       };
     };
 
+    // Generating all the cactus
     creatingElements(
       // Elements
       20,
@@ -79,6 +83,27 @@ const DesertGenerator: FC = () => {
       // Targeted array
       newCactus,
     );
+    
+    // Generating all the dunes
+    creatingElements(
+      // Elements
+      100,
+      // Element name
+      'Dune',
+      // Y axis rotation
+      (Math.PI * Math.random() * 2),
+      // Distance
+      100,
+      // Minimum scale
+      0.25,
+      // Maximum scale
+      2,
+      // Targeted array
+      newRocks,
+    );
+    
+
+    // Generating all the rocks
     creatingElements(
       // Elements
       100,
@@ -91,22 +116,42 @@ const DesertGenerator: FC = () => {
       // Minimum scale
       0.25,
       // Maximum scale
-      10,
+      2,
       // Targeted array
       newRocks,
     );
 
     setCactus(newCactus);
+    setDunes(newDunes);
     setRocks(newRocks);
-  }, [setCactus, setCliffYRotation, setRocks]);
+  }, [setCactus, setDunes, setCliffYRotation, setRocks]);
 
   return (
     <>
       {/* Creates as many rocks there is in the "rocks" state */}
       {
-        (rocks && rocks.length > 0
+        (cactus && cactus.length > 0
         ) ? (
-          rocks.map((r: PropsType, index: number) => {
+          cactus.map((c: PropsType, index: number) => {
+            return (
+              <Animation
+                key={index}
+                title={c.title}
+                position={c.position}
+                rotation={c.rotation}
+                scale={c.scale}
+              />
+            );
+          })
+        ) : (
+          null
+        )
+      }
+      {/* Creates as many dunes there is in the "dunes" state */}
+      {
+        (dunes && dunes.length > 0
+        ) ? (
+          dunes.map((r: PropsType, index: number) => {
             return (
               <Props
                 key={index}
@@ -123,16 +168,16 @@ const DesertGenerator: FC = () => {
       }
       {/* Creates as many rocks there is in the "rocks" state */}
       {
-        (cactus && cactus.length > 0
+        (rocks && rocks.length > 0
         ) ? (
-          cactus.map((c: PropsType, index: number) => {
+          rocks.map((r: PropsType, index: number) => {
             return (
-              <Animation
+              <Props
                 key={index}
-                title={c.title}
-                position={c.position}
-                rotation={c.rotation}
-                scale={c.scale}
+                title={r.title}
+                position={r.position}
+                rotation={r.rotation}
+                scale={r.scale}
               />
             );
           })
